e2f07650bea533d861f3c647ed2785832b103eac
zolangi <zolangiramirez@gmail.com>
2017-11-08 18:48:30 +0000

    Update files

--- cppLabs/lab8/FamilyTree.h
+++ cppLabs/lab8/FamilyTree.h
@@ -2,8 +2,74 @@
 #define FAMILYTREE
 
 #include <iostream>
+#include <vector>
+#include "FamilyTreeNode.h"
 using namespace std;
 
-struct 
+class FamilyTree{
+ public:
+  FamilyTreeNode *parent1;
+  FamilyTreeNode *parent2;
+  vector<Person *> children;
+  FamilyTree(string familyNameIn, FamilyTreeNode *parent1, FamilyTreeNode *parent2);
+ private:
+  string familyName;
+};
+
+//typdef struct node{
+//  FamilyTreeNode ftn;
+//  vector<Person *> children;
+//  struct node* next;
+//} node;
+
+/*
+    create a new node
+    initialize the data and next field
+ 
+    return the newly created node
+*/
+//node* create(int data,node* next);
+/*
+    add a new node at the beginning of the list
+*/
+//node* prepend(node* head,int data);
+/*
+    add a new node at the end of the list
+*/
+//node* append(node* head, int data);
+/*
+    insert a new node after the prev node
+*/
+//node* insert_after(node *head, int data, node* prev);
+/*
+    insert a new node before the nxt node
+*/
+//node* insert_before(node *head, int data, node* nxt);
+/*
+    traverse the linked list
+*/
+//void traverse(node* head);
+/*
+    remove node from the front of list
+*/
+//node* remove_front(node* head);
+/*
+    display a node
+*/
+//void display(node* n);
+//node* search(node* head,int data);
+/*
+    remove all element of the list
+*/
+//void clear(node *head);
+/*
+    return the number of elements in the list
+*/
+//int count(node *head);
+/*
+    sort the linked list using insertion sort
+*/
+//node* insertion_sort(node* head);
+
 
 #endif
--- cppLabs/lab8/FamilyTreeNode.h
+++ cppLabs/lab8/FamilyTreeNode.h
@@ -3,21 +3,14 @@
 #ifndef FAMILYTREENODE
 #define FAMILYTREENODE
 #include <iostream>
-#include "person.h"
+
 using namespace std;
 
-class FamilyTreeNode : public Person {
+class FamilyTreeNode{
  public:
-  FamilyTreeNode(string firstNameIn, string lastNameIn, FamilyTreeNode root1, FamilyTreeNode root2, int yearIn, int monthIn, int dayIn, char genderIn);
-  FamilyTreeNode getParent1() const;
-  FamilyTreeNode getParent2() const;
-
-  void setParent1(FamilyTreeNode roo1);
-  void setParent2(FamilyTreeNode root2);
-  
- private:
-  FamilyTreeNode parent1;
-  FamilyTreeNode parent2;
+  FamilyTreeNode *parent1;
+  FamilyTreeNode *parent2;
+  FamilyTreeNode(string firstNameIn, string lastNameIn, FamilyTreeNode *parent1, FamilyTreeNode *parent2, int yearIn, int monthIn, int dayIn, char genderIn);
 };
 
 #endif
