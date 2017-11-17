63ddb0fdcd562bc027dd20e6a325ca4ba3554abf
Zolangi Ramirez <zolangi@users.noreply.github.com>
2017-11-17 09:10:32 -0800

    Update anagram.py

--- pyLabs/lab10/anagram.py
+++ pyLabs/lab10/anagram.py
@@ -1,1 +1,11 @@
-# Lab 10: Anagrams
+# Anagramizer Class
+
+class Anagramizer:
+    def __init__(self, word):
+        self._word = word
+        
+    def get_word(self):
+        return self._word
+    
+    def __str__(self):
+        return 'Word: ' + str(self._word)
