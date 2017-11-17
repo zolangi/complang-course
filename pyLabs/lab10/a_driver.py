0bfcc2c14e5d3256e26bb019274a89feb83efbe7
Zolangi Ramirez <zolangi@users.noreply.github.com>
2017-11-17 09:20:22 -0800

    a_driver.py
    Anagramizer Driver Script

---
+++ pyLabs/lab10/a_driver.py
@@ -0,0 +1,12 @@
+# Driver for the Anagramizer
+
+import sys
+from anagram import Anagramizer
+
+def main():
+    anagram = Anagramizer()
+    
+    print(anagram, '\n')
+    
+if __name__ == "__main__":
+    sys.exit(main())
